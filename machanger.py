9083417bdbd616c97ae136e7771a3b56a109f231
Daroga1107 <daroga.1107@gmail.com>
2020-05-13 17:07:28 -0500

    est9ysjdh

--- machanger.py
+++ machanger.py
@@ -1,5 +1,13 @@
 import subprocess
 
-subprocess.call("ifconfig wlp3s0 down",shell=True)
-subprocess.call("ifconfig wlp3s0 hw ether 00:11:22:33:44:57",shell=True)
-subprocess.call("ifconfig wlp3s0 up",shell=True)
+interface = "wlp3s0"
+new_mac="11:22:33:44:77"
+
+print("[+] CHANGING MAC ADRESS FOR  " + interface + "to " + new_mac)
+
+
+#subprocess.call("ifconfig wlp3s0 down",shell=True)
+#subprocess.call("ifconfig wlp3s0 hw ether 00:11:22:33:44:57",shell=True)
+#subprocess.call("ifconfig wlp3s0 up",shell=True)
+
+
