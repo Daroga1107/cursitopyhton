890731ddb07c9e76f70af0d15251b127a4469363
Daroga1107 <daroga.1107@gmail.com>
2020-05-13 18:03:30 -0500

    ayuda para los comandos

--- machanger.py
+++ machanger.py
@@ -1,12 +1,20 @@
 import subprocess
+import optparse
+
 #"wlp3s0"
-interface = input("interface > ") 
-new_mac = input("nueva mac > ")
+parser = optparse.OptionParser()
+
+parser.add_option("-i","--interface", dest="interface", help="interface to change its MAC address")
+parser.add_option("-m","--mac", dest="new_mac", help="new mac address")
+
+(options, arguments ) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
 
 print("[+] CHANGING MAC ADRESS FOR " + interface + " to " + new_mac)
-
-
 
 subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
 subprocess.call(["ifconfig", interface, "up"])
+
