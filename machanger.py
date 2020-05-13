d0b9f2ca74f0b2e1d15210f938b1cd861290d8a2
Daroga1107 <daroga.1107@gmail.com>
2020-05-13 17:17:53 -0500

    variables

--- machanger.py
+++ machanger.py
@@ -1,13 +1,13 @@
 import subprocess
 
 interface = "wlp3s0"
-new_mac="11:22:33:44:77"
+new_mac="00:22:33:44:77:55"
 
-print("[+] CHANGING MAC ADRESS FOR  " + interface + "to " + new_mac)
+print("[+] CHANGING MAC ADRESS FOR " + interface + " to " + new_mac)
 
 
-#subprocess.call("ifconfig wlp3s0 down",shell=True)
-#subprocess.call("ifconfig wlp3s0 hw ether 00:11:22:33:44:57",shell=True)
-#subprocess.call("ifconfig wlp3s0 up",shell=True)
+subprocess.call("ifconfig "+interface + " down",shell=True)
+subprocess.call("ifconfig "+interface +" hw ether "+ new_mac,shell=True)
+subprocess.call("ifconfig "+interface +" up",shell=True)
 
 
