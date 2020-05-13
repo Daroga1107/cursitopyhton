d04d7dd04ea6d5257972140853f7d42a005f66ed
Daroga1107 <daroga.1107@gmail.com>
2020-05-13 18:29:09 -0500

    funciones y return

--- machanger.py
+++ machanger.py
@@ -1,20 +1,27 @@
 import subprocess
 import optparse
 
+def get_arguments():
+    parser = optparse.OptionParser()
+
+    parser.add_option("-i","--interface", dest="interface", help="interface to change its MAC address")
+    parser.add_option("-m","--mac", dest="new_mac", help="new mac address")
+
+    return  parser.parse_args()
+
+def  change_mac(interface, new_mac):
+        print("[+] CHANGING MAC ADRESS FOR " + interface + " to " + new_mac)
+
+        subprocess.call(["ifconfig", interface, "down"])
+        subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+        subprocess.call(["ifconfig", interface, "up"])
+
+
 #"wlp3s0"
-parser = optparse.OptionParser()
 
-parser.add_option("-i","--interface", dest="interface", help="interface to change its MAC address")
-parser.add_option("-m","--mac", dest="new_mac", help="new mac address")
 
-(options, arguments ) = parser.parse_args()
-
-interface = options.interface
-new_mac = options.new_mac
-
-print("[+] CHANGING MAC ADRESS FOR " + interface + " to " + new_mac)
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+#interface = options.interface
+#new_mac = options.new_mac
+(options, arguments ) = get_arguments()
+change_mac(options.interface, options.new_mac)
 
